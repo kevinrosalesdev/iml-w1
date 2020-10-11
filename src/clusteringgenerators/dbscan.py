41e8d5aab3e9068743255d570de73125a731366b
albaporras <66181309+albaporras@users.noreply.github.com>
2020-10-11 21:40:51 +0200

    Update dbscan.py

--- src/clusteringgenerators/dbscan.py
+++ src/clusteringgenerators/dbscan.py
@@ -1,2 +1,15 @@
-def test_dbscan():
-    return 1
+from sklearn.cluster import DBSCAN
+from collections import Counter
+
+
+def test_dbscan(dataset):
+    dbscan = DBSCAN(eps=3, min_samples=4, metric='euclidean')
+
+    model = dbscan.fit(dataset)
+
+    labels = model.labels_
+    clusters = Counter(labels)
+    # 'the id -1 contains the outliers
+    print("Clusters id and the points inside", clusters)
+    print('Num of clusters = {}'.format(len(clusters)-1))
+    return model

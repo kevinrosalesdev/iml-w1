8901a95a1905e1f5e7354530f4e79cc43803ac13
kevinrosalesdev <kevinrosalesdev@gmail.com>
2020-10-12 16:27:26 +0200

    structure improvements

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from arffdatasetreader import dataset_reader as dr
 from clusteringgenerators import dbscan
+from collections import Counter
 
-num_ds = dr.read_processed_data('numerical', False)
-cat_ds = dr.read_processed_data('categorical', False)
-mix_ds = dr.read_processed_data('mixed', False)
-print(num_ds.head())
-print(cat_ds.head())
-print(mix_ds.head())
 
-print(dbscan.test_dbscan(num_ds))
-print(dbscan.test_dbscan(cat_ds))
-print(dbscan.test_dbscan(mix_ds))
+def get_data():
+    num_ds = dr.read_processed_data('numerical', False)
+    cat_ds = dr.read_processed_data('categorical', False)
+    mix_ds = dr.read_processed_data('mixed', False)
+    return num_ds, cat_ds, mix_ds
+
+
+def test_dbscan(dataset, eps=3, min_samples=4):
+    print(dataset.head())
+    model = dbscan.ul_dbscan(dataset, eps, min_samples)
+    labels = model.labels_
+    clusters = Counter(labels)
+    # 'the id -1 contains the outliers
+    print("Clusters id and the points inside", clusters)
+    print('Num of clusters = {}'.format(len(clusters)-1))
+
+
+if __name__ == '__main__':
+    num_ds, cat_ds, mix_ds = get_data()
+
+    # DBScan testing section
+    test_dbscan(num_ds)

b47ed56bcfbfafd6acc786a352a563278ecf2d1c
kevinrosalesdev <kevinrosalesdev@gmail.com>
2020-10-12 21:09:45 +0200

    parameters optimization

--- src/clusteringgenerators/dbscan.py
+++ src/clusteringgenerators/dbscan.py
@@ -1,7 +1,33 @@
 from sklearn.cluster import DBSCAN
+from sklearn.metrics.pairwise import euclidean_distances
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 def ul_dbscan(dataset, eps, min_samples):
-    dbscan = DBSCAN(eps=3, min_samples=4, metric='euclidean')
+    if min_samples < dataset.shape[1] + 1 or min_samples < 3:
+        print("[WARNING] 'min_samples' parameter should be greater than D(features) + 1 and at least 3")
+
+    print("Parameters (eps=" + str(eps) + ", min_samples=" + str(min_samples) + ")")
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean')
     model = dbscan.fit(dataset)
     return model
+
+
+def plot_k_neighbor_distance(dataset, k):
+    np_dataset = dataset.to_numpy()
+    k_nnd = np.zeros((np_dataset.shape[0]))
+    for index, i in enumerate(np_dataset):
+        distances = euclidean_distances([i], np.delete(np_dataset, index, axis=0))
+        k_nnd[index] = np.sort(distances[0])[k-1]
+
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    plt.minorticks_on()
+    plt.title("kth Nearest Neighbor Distance (k=" + str(k) + ")")
+    plt.xlabel("Points Sorted according to Distance of " + str(k) + "th Nearest Neighbor")
+    plt.ylabel(str(k) + "th Nearest Neighbor Distance")
+    h_outliers_count = k_nnd[k_nnd > np.percentile(k_nnd, 95)].shape[0]
+    plt.plot(list(range(0, dataset.shape[0] - h_outliers_count)),
+             np.sort(k_nnd)[:-h_outliers_count], marker=".")
+    plt.show()

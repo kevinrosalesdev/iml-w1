c378ccb0d767e0f858d3ee66295063b6e5b87acf
kevinrosalesdev <kevinrosalesdev@gmail.com>
2020-10-30 19:36:12 +0100

    quick fix with for in metrics

--- src/validators/metrics.py
+++ src/validators/metrics.py
@@ -11,7 +11,7 @@ def get_cf_and_pca(dataset, targets, algorithm='b-kmeans'):
     real_k = [10, 18, 4]
 
     if algorithm == "b-kmeans":
-        for index in range(1, len(best_k_bisecting) - 1):
+        for index in range(0, len(best_k_bisecting)):
             target_labels = targets[index]
 
             tic = time.time()
